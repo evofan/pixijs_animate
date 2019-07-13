fc87a2c48c2a630412aa6795d229202a7764bdbb
evofan <evofan@outlook.jp>
2019-07-14 02:56:55 +0900

    add flying bird and shooting star

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 ﻿let scene = new PIXI.animate.Scene(384, 384, {
     view: document.getElementById("stage"),
-    backgroundColor: 0xffffff,
+    backgroundColor: 0x000000,
     antialias: true
 });
 scene.load(lib.fall_leaf_04_pixianimate);
 
 let scene2 = new PIXI.animate.Scene(384, 384, {
     view: document.getElementById("stage2"),
-    backgroundColor: 0x0,
+    backgroundColor: 0x000000,
     antialias: true
 });
 scene2.load(lib.fish_08_PixiAnimate);
---
+++ src/main2.js
@@ -0,0 +1,13 @@
+﻿let scene3 = new PIXI.animate.Scene(384, 384, {
+    view: document.getElementById("stage3"),
+    backgroundColor: 0x000000,
+    antialias: true
+});
+scene3.load(lib.flying_bird_02_PixiAnimate);
+
+let scene4 = new PIXI.animate.Scene(384, 384, {
+    view: document.getElementById("stage4"),
+    backgroundColor: 0x000000,
+    antialias: true
+});
+scene4.load(lib.shooting_star_03_PixiAnimate);
